d6a49c9defc332afa0c1c3b0b1e62c2caa02285e
Nandaja Varma <nandaja.varma@gmail.com>
2017-02-15 10:24:31 -0500

    Space age

--- first_scripts/lib/first_scripts.ex
+++ first_scripts/lib/first_scripts.ex
@@ -56,9 +56,34 @@ defmodule FirstScripts do
   def hello(name \\ "World") do
       "Hello, " <> "#{name}!"
   end
+
+
+  @type planet :: :mercury | :venus | :earth | :mars | :jupiter
+                | :saturn | :uranus | :neptune
+
+  @doc """
+  Return the number of years a person that has lived for 'seconds' seconds is
+  aged on 'planet'.
+  """
+  @planet_years %{:earth => 1,
+                  :mercury => 0.2408467,
+                  :venus =>  0.61519726,
+                  :mars => 1.8808158,
+                  :jupiter => 11.862615,
+                  :saturn => 29.447498,
+                  :uranus => 84.016846,
+                  :neptune => 164.79132}
+
+  @year_to_secs 31557600
+
+  @spec age_on(planet, pos_integer) :: float
+  def age_on(planet, seconds) do
+    seconds / (@year_to_secs * @planet_years[planet])
+  end
 end
 
 
 FirstScripts.fib(10)
 FirstScripts.hello()
 FirstScripts.histogram('AAAA')
+FirstScripts.age_on(:earth, 1_000_000_000)
--- first_scripts/test/first_scripts_test.exs
+++ first_scripts/test/first_scripts_test.exs
@@ -57,4 +57,59 @@ defmodule FirstScriptsTest do
   test "says hello other sample name" do
     assert FirstScripts.hello("Bob") == "Hello, Bob!"
   end
+
+  # @tag :pending
+  test "age on Earth" do
+    input = 1_000_000_000
+    assert_in_delta 31.69, FirstScripts.age_on(:earth, input), 0.005
+  end
+
+  #@tag :pending
+  test "age on Mercury" do
+    input = 2_134_835_688
+    assert_in_delta 67.65, FirstScripts.age_on(:earth, input), 0.005
+    assert_in_delta 280.88, FirstScripts.age_on(:mercury, input), 0.005
+  end
+
+  #@tag :pending
+  test "age on Venus" do
+    input = 189_839_836
+    assert_in_delta 6.02, FirstScripts.age_on(:earth, input), 0.005
+    assert_in_delta 9.78, FirstScripts.age_on(:venus, input), 0.005
+  end
+
+  #@tag :pending
+  test "age on Mars" do
+    input = 2_329_871_239
+    assert_in_delta 73.83, FirstScripts.age_on(:earth, input), 0.005
+    assert_in_delta 39.25, FirstScripts.age_on(:mars, input), 0.005
+  end
+
+  #@tag :pending
+  test "age on Jupiter" do
+    input = 901_876_382
+    assert_in_delta 28.58, FirstScripts.age_on(:earth, input), 0.005
+    assert_in_delta 2.41, FirstScripts.age_on(:jupiter, input), 0.005
+  end
+
+  #@tag :pending
+  test "age on Saturn" do
+    input = 3_000_000_000
+    assert_in_delta 95.06, FirstScripts.age_on(:earth, input), 0.005
+    assert_in_delta 3.23, FirstScripts.age_on(:saturn, input), 0.005
+  end
+
+  #@tag :pending
+  test "age on Uranus" do
+    input = 3_210_123_456
+    assert_in_delta 101.72, FirstScripts.age_on(:earth, input), 0.005
+    assert_in_delta 1.21, FirstScripts.age_on(:uranus, input), 0.005
+  end
+
+  #@tag :pending
+  test "age on Neptune" do
+    input = 8_210_123_456
+    assert_in_delta 260.16, FirstScripts.age_on(:earth, input), 0.005
+    assert_in_delta 1.58, FirstScripts.age_on(:neptune, input), 0.005
+  end
 end
