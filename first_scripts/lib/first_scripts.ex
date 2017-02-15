d3b67f6024c884d84d1333fd2df8a2b8302692a8
Nandaja Varma <nandaja.varma@gmail.com>
2017-02-14 19:30:05 -0500

    Done some more simple ones

--- first_scripts/lib/first_scripts.ex
+++ first_scripts/lib/first_scripts.ex
@@ -22,7 +22,43 @@ defmodule FirstScripts do
       Enum.take @start, n
     end
   end
+
+  @nucleotides [?A, ?C, ?G, ?T]
+
+  @doc """
+  Counts individual nucleotides in a NucleotideCount strand.
+
+  ## Examples
+
+  """
+  @spec count([char], char) :: non_neg_integer
+  def count(strand, nucleotide) do
+      Enum.count(strand, &(&1 == nucleotide))
+  end
+
+
+  @doc """
+  Returns a summary of counts by nucleotide.
+
+  ## Examples
+
+  """
+  @spec histogram([char]) :: map
+  def histogram(strand) do
+    for n <- @nucleotides, into: %{}, do: {n, count(strand, n)}
+  end
+
+  @doc """
+  Greets the user by name, or by saying "Hello, World!"
+  if no name is given.
+  """
+  @spec hello(String.t) :: String.t
+  def hello(name \\ "World") do
+      "Hello, " <> "#{name}!"
+  end
 end
 
 
 FirstScripts.fib(10)
+FirstScripts.hello()
+FirstScripts.histogram('AAAA')
--- first_scripts/test/first_scripts_test.exs
+++ first_scripts/test/first_scripts_test.exs
@@ -9,4 +9,52 @@ defmodule FirstScriptsTest do
   test "Wrong the fibonacci of 5" do
     refute FirstScripts.fib(5) == [0, 1, 1, 2, 3, 5]
   end
+  #
+  # @tag :pending
+  test "empty dna string has no adenine" do
+    assert FirstScripts.count('', ?A) == 0
+  end
+
+  #@tag :pending
+  test "repetitive cytosine gets counted" do
+    assert FirstScripts.count('CCCCC', ?C) == 5
+  end
+
+  #@tag :pending
+  test "counts only thymine" do
+    assert FirstScripts.count('GGGGGTAACCCGG', ?T) == 1
+  end
+
+  #@tag :pending
+  test "empty dna string has no nucleotides" do
+    expected = %{?A => 0, ?T => 0, ?C => 0, ?G => 0}
+    assert FirstScripts.histogram('') == expected
+  end
+
+  #@tag :pending
+  test "repetitive sequence has only guanine" do
+    expected = %{?A => 0, ?T => 0, ?C => 0, ?G => 8}
+    assert FirstScripts.histogram('GGGGGGGG') == expected
+  end
+
+  #@tag :pending
+  test "counts all nucleotides" do
+    s = 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
+    expected = %{?A => 20, ?T => 21, ?C => 12, ?G => 17}
+    assert FirstScripts.histogram(s) == expected
+  end
+
+  test "says hello with no name" do
+    assert FirstScripts.hello() == "Hello, World!"
+  end
+
+  @tag :pending
+  test "says hello sample name" do
+    assert FirstScripts.hello("Alice") == "Hello, Alice!"
+  end
+
+  @tag :pending
+  test "says hello other sample name" do
+    assert FirstScripts.hello("Bob") == "Hello, Bob!"
+  end
 end
